917cfc70f915892482b88a39fc8169d15449f9b3
silva4dev <web4dev.lucas@outlook.com>
2023-03-05 00:30:08 -0300

    inicial

---
+++ README.md
@@ -0,0 +1,10 @@
+# sails-typescript
+
+This sample is created following the [Using TypeScript in a Sails app](http://sailsjs.com/documentation/tutorials/using-type-script) tutorial.
+
+## Steps
+
+* Clone it
+* `npm install`
+* `node app.js` for Development or `node.app --prod` for Production
+* Navigate to `http://127.0.0.1:1337/ts`
---
+++ api/controllers/TsController.ts
@@ -0,0 +1,17 @@
+// api/controllers/TsController.js
+
+import util = require('util');
+import express = require('express');
+
+declare var sails: any;
+
+export function index(req:any, res:any, next: Function):any {
+  console.log('index() from TsController.ts');
+  res.status(200).send('Hello from Typescript!');
+}
+
+export function config(req: express.Request, res: express.Response, next: Function) {
+  console.log('config() from TsController.ts');
+  res.status(200)
+     .send('<h1>sails.config :</h1><pre>' + util.inspect(sails.config) + '<pre>');
+}
